ec0485ee22bf21f8be7f5e6cae3c590197cb03bf
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-07-10 02:46:37 -0400

    add a simple test to check that paths are rewritten

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -276,3 +276,9 @@ class TestStorageWrite(TestStorageRucioBase):
         assert "already exists on Rucio" not in caplog.text
         obj.store_object()
         assert "already exists on Rucio" in caplog.text
+
+    def test_path_rewriting(self, tmp_path: Path) -> None:
+        """Test that forbidden characters in queries get replaced."""
+        scope = SITE_CONFIG["scope"]
+        obj = self.get_storage_object(tmp_path, query=f"rucio://{scope}/path/normally/not/allowed")
+        assert obj.file == "path-2F-normally-2F-not-2F-allowed"
